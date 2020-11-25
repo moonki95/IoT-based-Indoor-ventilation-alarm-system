526fe6b61b8512fb4768fff6887dcc7c5ea972a3
yeziwoo <wyz9088@gmail.com>
2020-11-25 20:38:25 +0900

    Added notification

--- Android3/app/src/main/java/com/example/Android/MainActivity.java
+++ Android3/app/src/main/java/com/example/Android/MainActivity.java
@@ -1,25 +1,38 @@
 package com.example.Android;
 
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationCompat;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.lang.*;
 import java.net.URL;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -36,6 +49,10 @@ public class MainActivity extends AppCompatActivity {
 
     private OutdoorAir outdoorAir =new OutdoorAir();
 
+    private Timer timer=new Timer();
+    private static String IP_ADDRESS = "54.144.159.63";
+    private static String TAG = "phptest";
+    private String url = "http://" + IP_ADDRESS + "/getjson.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +67,16 @@ public class MainActivity extends AppCompatActivity {
         textViewSido = (TextView) findViewById(R.id.textView_sido);
         textViewPm10 = (TextView) findViewById(R.id.textView_pm10);
         textViewPm25 = (TextView) findViewById(R.id.textView_pm25);
+
+        //타이머
+        TimerTask tt= new TimerTask(){
+            @Override
+            public void run() {
+                SelectDatabaseTask task = new SelectDatabaseTask(url, null);
+                task.execute();
+            }
+        };
+        timer.schedule(tt,0,5000);  //5초마다 실행
 
        /* // GPS 접근 권한 확인???
         if (!checkLocationServicesStatus()) {
@@ -84,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent;
         switch(item.getItemId()){
             case R.id.menu_outdoor_current_position:
-                // 현재위치 측정해서 결과 표시
+
                 break;
             case R.id.menu_outdoor_diff_region:
                 intent=new Intent(this, OutdoorPopupActivity.class);
@@ -114,6 +141,170 @@ public class MainActivity extends AppCompatActivity {
                 GetXMLTask getXMLTask = new GetXMLTask();
                 getXMLTask.execute();
             }
+        }
+    }
+
+    private void createNotification(String text) {
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "default");
+
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("실내 미세먼지");
+        builder.setContentText(text);
+
+        builder.setColor(Color.RED);
+        // 사용자가 탭을 클릭하면 자동 제거
+        builder.setAutoCancel(true);
+
+        // 알림 표시
+        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(new NotificationChannel("default", "기본 채널", NotificationManager.IMPORTANCE_DEFAULT));
+        }
+
+        // id값은
+        // 정의해야하는 각 알림의 고유한 int값
+        notificationManager.notify(1, builder.build());
+    }
+
+    class SelectDatabaseTask extends AsyncTask<Void, Void, String> {
+
+        private String url1;
+        private ContentValues values1;
+        String result1; // 요청 결과를 저장할 변수.
+
+        public SelectDatabaseTask(String url, ContentValues contentValues) {
+            this.url1 = url;
+            this.values1 = contentValues;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+            result1 = requestHttpURLConnection.request(url1, values1); // 해당 URL로 부터 결과물을 얻어온다.
+            return result1; // 여기서 당장 실행 X, onPostExcute에서 실행
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            GetAirData(s); // 파서로 전체 출력
+        }
+    }
+
+
+    private void GetAirData(String mJsonString){
+
+        String TAG_JSON="result";
+        String TAG_TEMP = "temp";
+        String TAG_HUMID ="humid";
+        String TAG_DUST="dust";
+        String _temp = null;
+        String _humid=null;
+        String _dust=null;
+
+        String title, text;
+
+        try {
+            JSONObject jsonObject = new JSONObject(mJsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+            JSONObject item = jsonArray.getJSONObject(0);
+
+            _temp = item.getString(TAG_TEMP);
+            _humid = item.getString(TAG_HUMID);
+            _dust = item.getString(TAG_DUST);
+
+            int dust = Integer.parseInt(_dust);
+            if (80 < dust && dust <= 120) {
+                text = "실내 공기가 약간나쁨이에요. 환기를 해보세요~";
+                createNotification(text);
+            }
+            else if(dust > 120){
+                text = "실내 공기가 나쁨이에요. 환기가 필요해요!!";
+                createNotification(text);
+            }
+
+
+        } catch (JSONException e) {
+
+            Log.d(TAG, "showResult : ", e);
+        }
+    }
+
+    private class GetXMLTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?serviceKey=8%2B64ddNqAnHmK8Lx8tI%2Bngxj6dtexb%2FhgwcSwsDjWb%2FbWSgRHUmNk9%2BtkAlqPCFcVb1QiMx49DQfXdMPHnNkzg%3D%3D&numOfRows=40&pageNo=1&sidoName="+sigungu[0]+"&searchCondition=DAILY";
+            XmlPullParserFactory factory;
+            XmlPullParser parser;
+            URL xmlUrl;
+            String returnResult = "";
+
+            boolean isdataTime = false, isCityName = false, ispm10 = false, ispm25 = false;
+
+            try {
+                int flag = 0;
+                int check=0;
+
+                xmlUrl = new URL(url);
+                xmlUrl.openConnection().getInputStream();
+                factory = XmlPullParserFactory.newInstance();
+                parser = factory.newPullParser();
+                parser.setInput(xmlUrl.openStream(), "utf-8");
+
+                int eventType = parser.getEventType();
+                while ((eventType != XmlPullParser.END_DOCUMENT) && check == 0) {
+                    //<tag>text</tag>이므로 startTag뒤에 있는 text를 찾기위한 switch절
+                    switch (eventType) {
+                        case XmlPullParser.START_TAG:  //start tag가 다음 중 하나일 때 flag=true
+                            if (parser.getName().equals("cityName")) {
+                                isCityName = true;
+                            } else if (parser.getName().equals("pm10Value")) {
+                                ispm10 = true;
+                            } else if (parser.getName().equals("pm25Value")) {
+                                ispm25 = true;
+                            }
+                            break;
+
+                        case XmlPullParser.TEXT:
+                            if(isCityName) {
+                                if(parser.getText().equals(sigungu[1])){
+                                    flag=1;
+                                }
+                                isCityName=false;
+                            }else if (ispm10&&flag==1) {
+                                outdoorAir.setPM10(parser.getText());
+                                ispm10 = false;
+                            } else if (ispm25&&flag==1) {
+                                outdoorAir.setPM25(parser.getText());
+                                ispm25 = false;
+                            }
+                            break;
+
+                        case XmlPullParser.END_TAG:
+                            if (parser.getName().equals("item")&&flag==1) {
+                                textViewSido.setText(sigungu[0]+" "+sigungu[1]);
+                                textViewPm10.setText(outdoorAir.getPM10()+" ㎍/㎥        "+ outdoorAir.getPM10Grade());
+                                textViewPm25.setText(outdoorAir.getPM25()+" ㎍/㎥        "+ outdoorAir.getPM25Grade());
+
+                                check = 1;
+                                flag=0;
+
+                            }
+                            break;
+                    }
+                    eventType = parser.next();
+                }
+            } catch (Exception e) {
+                textViewSido.setText("에러가 났습니다...");
+                textViewPm10.setText("에러가 났습니다...");
+                textViewPm25.setText("에러가 났습니다...");
+            }
+            return returnResult;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
         }
     }
 
@@ -228,89 +419,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }*/
-
-
-    private class GetXMLTask extends AsyncTask<Void, Void, String> {
-
-        @Override
-        protected String doInBackground(Void... voids) {
-            String url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?serviceKey=8%2B64ddNqAnHmK8Lx8tI%2Bngxj6dtexb%2FhgwcSwsDjWb%2FbWSgRHUmNk9%2BtkAlqPCFcVb1QiMx49DQfXdMPHnNkzg%3D%3D&numOfRows=40&pageNo=1&sidoName="+sigungu[0]+"&searchCondition=DAILY";
-            XmlPullParserFactory factory;
-            XmlPullParser parser;
-            URL xmlUrl;
-            String returnResult = "";
-
-            boolean isdataTime = false, isCityName = false, ispm10 = false, ispm25 = false;
-
-            try {
-                int flag = 0;
-                int check=0;
-
-                xmlUrl = new URL(url);
-                xmlUrl.openConnection().getInputStream();
-                factory = XmlPullParserFactory.newInstance();
-                parser = factory.newPullParser();
-                parser.setInput(xmlUrl.openStream(), "utf-8");
-
-                int eventType = parser.getEventType();
-                while ((eventType != XmlPullParser.END_DOCUMENT) && check == 0) {
-                    //<tag>text</tag>이므로 startTag뒤에 있는 text를 찾기위한 switch절
-                    switch (eventType) {
-                        case XmlPullParser.START_TAG:  //start tag가 다음 중 하나일 때 flag=true
-                            if (parser.getName().equals("cityName")) {
-                                isCityName = true;
-                            } else if (parser.getName().equals("pm10Value")) {
-                                ispm10 = true;
-                            } else if (parser.getName().equals("pm25Value")) {
-                                ispm25 = true;
-                            }
-                            break;
-
-                        case XmlPullParser.TEXT:
-                            if(isCityName) {
-                                if(parser.getText().equals(sigungu[1])){
-                                    flag=1;
-                                }
-                                isCityName=false;
-                            }else if (ispm10&&flag==1) {
-                                outdoorAir.setPM10(parser.getText());
-                                ispm10 = false;
-                            } else if (ispm25&&flag==1) {
-                                outdoorAir.setPM25(parser.getText());
-                                ispm25 = false;
-                            }
-                            break;
-
-                        case XmlPullParser.END_TAG:
-                            if (parser.getName().equals("item")&&flag==1) {
-                                textViewSido.setText(sigungu[0]+" "+sigungu[1]);
-                                textViewPm10.setText(outdoorAir.getPM10()+" ㎍/㎥        "+ outdoorAir.getPM10Grade());
-                                textViewPm25.setText(outdoorAir.getPM25()+" ㎍/㎥        "+ outdoorAir.getPM25Grade());
-
-                                check = 1;
-                                flag=0;
-
-                            }
-                            break;
-                    }
-                    eventType = parser.next();
-                }
-            } catch (Exception e) {
-                textViewSido.setText("에러가 났습니다...");
-                textViewPm10.setText("에러가 났습니다...");
-                textViewPm25.setText("에러가 났습니다...");
-            }
-            return returnResult;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-        }
-    }
-
-
-
-
 
 //    private void WindowCtrl(String _pm10,String _dust){
 //        int pm10=Integer.parseInt(_pm10);
